e8ad6d956022d4e775dcc115c5e3c3d9b7667836
Cong Phu <cong.phu@terralogic.com>
2017-07-27 14:40:26 +0700

    Initial Commit

---
+++ IAPSubscription/IAPSubscription.h
@@ -0,0 +1,19 @@
+//
+//  IAPSubscription.h
+//  IAPSubscription
+//
+//  Created by Cong Phu on 7/27/17.
+//  Copyright © 2017 Cong Phu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IAPSubscription.
+FOUNDATION_EXPORT double IAPSubscriptionVersionNumber;
+
+//! Project version string for IAPSubscription.
+FOUNDATION_EXPORT const unsigned char IAPSubscriptionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IAPSubscription/PublicHeader.h>
+
+
